1f9b2bfb33c45c5ec97b7487ea72e6722d3544e7
DreamNya <34838824+DreamNya@users.noreply.github.com>
2023-01-29 10:56:47 +0800

    [v0.10.9] Live website

--- src/modules/index.ts
+++
@@ -1,8 +0,0 @@
-// Get our polyfills loaded first
-import './polyfill';
-
-// Ensure that the Knockout Extenders are injected
-import './koExtenders';
-
-// Load everything else
-import './temporaryWindowInjection';
