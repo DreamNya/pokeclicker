0f6ee8d76b0087c67ff7a11ecfe2ae7c79aaf6fe
DreamNya <34838824+DreamNya@users.noreply.github.com>
2023-02-10 21:52:20 +0800

    Update translations
    Update translations

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -42,7 +42,7 @@ config = Object.assign({
     FEATURE_FLAGS: {
         preloadUnreleasedTowns: false,
     },
-    TRANSLATIONS_URL: 'https://translations.pokeclicker.com',
+    TRANSLATIONS_URL: 'https://dreamnya.github.io/pokeclicker-translations/',
 }, config);
 
 const escapeRegExp = (string) => {
