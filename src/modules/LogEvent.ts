fbfcda5d4a92d64957f11fcfd96de466bb3d250e
DreamNya <34838824+DreamNya@users.noreply.github.com>
2023-08-31 13:54:59 +0800

    移除统计跟踪函数

--- src/modules/LogEvent.ts
+++ src/modules/LogEvent.ts
@@ -1,15 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // For helper functions that may be needed across all files
 // TODO: Convert this to not be a class after everything is TS modules
 const LogEvent = (event: string, event_category: string, event_label?: string, value?: number) => {
-    try {
-        gtag('event', event, {
-            event_category,
-            event_label,
-            value,
-        });
-    } catch (e) {
-        console.warn('Failed to send event data.');
-    }
+    // 禁止统计跟踪
+    // Do Nothing
 };
 
 export default LogEvent;
