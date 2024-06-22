b8882332f38f34a03d33a5314019faa1a245ba32
DreamNya <34838824+DreamNya@users.noreply.github.com>
2024-06-22 14:48:28 +0000

    Deploying to master from @ DreamNya/pokeclicker@26063943945128d5941b2938c9728fa4c57392d3 🚀

--- libs/core.js
+++ libs/core.js
@@ -175,7 +175,6 @@ transTaskMgr = {
 function TransSubTextNode(node) {
     if (node.childNodes.length > 0) {
         for (let subnode of node.childNodes) {
-            if ($(subnode).parents("#dungeonPokemonList").length > 0) break;
             if (subnode.nodeName === "#text") {
                 let text = subnode.textContent;
                 let cnText = cnItem(text, subnode);
@@ -223,7 +222,6 @@ function TransSubTextNode(node) {
             } else if (mutation.addedNodes.length > 0) {
                 for (let node of mutation.addedNodes) {
                     if (node.nodeName === "#text") {
-                        if ($(node).parents("#dungeonPokemonList").length > 0) break;
                         node.textContent = cnItem(node.textContent, node);
                         //console.log(node);
                     } else if (node.nodeName !== "SCRIPT" && node.nodeName !== "STYLE" && node.nodeName !== "TEXTAREA") {
